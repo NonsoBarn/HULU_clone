2a66ff41dd2777d85c164b502964af31766fe44f
NonsoBarn <102404755+NonsoBarn@users.noreply.github.com>
2023-05-08 11:28:41 +0100

    final commit

--- hulu/pages/index.js
+++ hulu/pages/index.js
@@ -2,11 +2,12 @@ import Head from "next/head";
 
 import { Inter } from "next/font/google";
 import Header from "@/components/Header";
+import requests from "@/utils/requests";
 
 import Nav from "@/components/Nav";
 import Results from "@/components/Results";
 
-export default function Home() {
+export default function Home({ results }) {
   return (
     <>
       <Head>
@@ -17,11 +18,23 @@ export default function Home() {
       </Head>
       <Header />
       <Nav />
-      <Results />
+      <Results results={results} />
     </>
   );
 }
 
 export async function getServerSideProps(context) {
   const genre = context.query.genre;
+
+  const request = await fetch(
+    `https://api.themoviedb.org/3${
+      requests[genre]?.url || requests.fetchTrending.url
+    }`
+  ).then((res) => res.json());
+
+  return {
+    props: {
+      results: request.results,
+    },
+  };
 }
