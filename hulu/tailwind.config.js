1cbf8c42d20ccc38b6988dfa81ec31a3bc3e494e
NonsoBarn <102404755+NonsoBarn@users.noreply.github.com>
2023-05-08 11:28:57 +0100

    Update tailwind.config.js

--- hulu/tailwind.config.js
+++ hulu/tailwind.config.js
@@ -9,7 +9,11 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        "3xl": "2000px",
+      },
+    },
   },
   plugins: [require("tailwind-scrollbar-hide")],
 };
